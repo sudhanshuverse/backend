20d355055b0013b38d97936bfc2929f077eddf75
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-02-19 20:32:11 +0530

    Added 01-introduction

---
+++ nodeJs/01-introduction/app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+        res.statusCode = 204;
+        return res.end();
+    }
+    res.end("<h1>Hello, How are you.</h1>")
+    console.log("URL", req.url);
+    console.log("Response", req.method);
+    console.log("Headers ",req.headers);
+    
+})
+
+const PORT = 8000;
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
