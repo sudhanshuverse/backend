ba2f75af163d513ccd5eaf43d850db3192c4a338
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-02-24 21:40:42 +0530

    Update app.js

--- nodeJs/02-request-response/app.js
+++ nodeJs/02-request-response/app.js
@@ -1,58 +1,43 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/favicon.ico') {
-        res.statusCode = 204;
-        return res.end();
+    console.log("URL:", req.url);
+    console.log("Method: ", req.method);
+    console.log("Header: ", req.headers);
+
+    if (req.url === '/') {
+        res.write("<h1>Home page</h1>")
+        res.write(`
+            <form action="/submit-details" method="GET">
+                <label for="name" value="name">Name: </label>
+                <input type="text" placeholder="Username" id="name" name="username" required/><br><br>
+                <label>Age: </label>
+                <input id="male" value="male" type="radio" name="age"/>
+                <label for="male">Male: </label>
+                <input type="radio" id="female" name="age" value="female"/>
+                <label for="female">Female:</label> <br><br>
+                <button>Submit</button>
+            </form>
+            `)
+        res.end();
+    } else if (req.url === '/about') {
+        res.write("<h1>About page</h1>")
+        res.end();
+    } else if (req.url === '/contact') {
+        res.write("<h1>Contact page</h1>")
+        res.end();
+    } else if (req.url === '/submit-details') {
+        res.write("<h1>Submit Details<h1>")
+        res.end();
     }
-    console.log("URL", req.url);
-    console.log("Response", req.method);
-    console.log("Headers ", req.headers);
-
-    res.setHeader('Content-Type', 'text/html');
-
-    if (req.url === '/submit-details') {
-        res.write(`
-        <head>
-            <title>Submit Page</title>
-        </head>
-        <body>
-            <h1>I am from submit details page.</h1>
-        </body>`)
-    } else if (req.url === '/men') {
-        res.write(`
-        <head>
-            <title>Men section</title>
-        </head>
-        <body>
-            <h1>I am from men section.</h1>
-        </body>`)
-    } else if (req.url === '/women') {
-        res.write(`
-        <head>
-            <title>Women section</title>
-        </head>
-        <body>
-            <h1>I am from women section.</h1>
-        </body>`)
+    else {
+        res.write("<h1>This is default page</h1>")
+        res.end();
     }
 
-    if (req.url === '/')
-        res.write(`<h1>Welcome to Home page</h1>`);
-    res.write(`<form action="/submit-details" method="POST">`);
-    res.write(`<input type="text" id="name" name="name" placeholder="Enter your name" /><br><br>`);
-    res.write(`<label for="gender">Gender :</label>`);
-    res.write(`<input type="radio" id="male" name="gender" value="male" />`);
-    res.write(`<label for="male">Male</label>`);
-    res.write(`<input type="radio" id="female" name="gender" value="female" />`);
-    res.write(`<label for="female">Female</label><br><br>`);
-    res.write(`<button type="submit">Submit</button>`);
-    res.write(`</form>`);
-    return res.end();
-
-});
+})
 
 const PORT = 8000;
 server.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+    console.log(`Server is running on: ${PORT}`);
 })
