11dec4bc78792874342c41a6d4bd9abf42927529
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-05 21:40:27 +0530

    Updated routes all files

--- expressJs/06-dynamic-paths/routes/storeRouter.js
+++ expressJs/06-dynamic-paths/routes/storeRouter.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const storeRouter = express.Router();
 
-const homesController = require('../controllers/storeController');
+const storeController = require('../controllers/storeController');
 
-storeRouter.get("/", homesController.getIndex);
-storeRouter.get("/homes", homesController.getHomes);
-storeRouter.get("/bookings", homesController.getBookings);
-storeRouter.get("/favourites", homesController.getFavoriteList);
+storeRouter.get("/", storeController.getIndex);
+storeRouter.get("/homes", storeController.getHomes);
+storeRouter.get("/bookings", storeController.getBookings);
+storeRouter.get("/favourites", storeController.getFavoriteList);
+
+storeRouter.get("/homes/:homeId", storeController.getHomeDetails);
+storeRouter.post("/favourites", storeController.postAddToFavorite);
+storeRouter.post("/favourites/delete/:homeId", storeController.postRemoveFromFavourite);
+
 
 module.exports = storeRouter;
