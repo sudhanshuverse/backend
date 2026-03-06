581b6ebdd5d2be3c052c69354bdfa42e85e6c797
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-06 23:02:09 +0530

    Initialize

---
+++ database/01-introduction-to-mysql/app.js
@@ -0,0 +1,29 @@
+// CORE MODULE
+const path = require('path');
+
+// EXTERNAL MODULE
+const express = require('express');
+
+// LOCAL MODULE
+const storeRouter = require('./routes/storeRouter');
+const hostRouter = require('./routes/hostRouter');
+const rootDir = require('./utils/pathUtil');
+const { ErrorPage } = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded());
+app.use(storeRouter);
+app.use("/host", hostRouter);
+
+app.use(express.static(path.join(rootDir, 'public')));
+
+app.use(ErrorPage)
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`);
+})
