ca12d61ec8382489bffb545f49464db003e81231
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-10 21:25:52 +0530

    Update the code for mongoose databse

--- database/03-introduction-to-mongoose/app.js
+++ database/03-introduction-to-mongoose/app.js
@@ -9,25 +9,27 @@ const storeRouter = require('./routes/storeRouter');
 const hostRouter = require('./routes/hostRouter');
 const rootDir = require('./utils/pathUtil');
 const { ErrorPage } = require('./controllers/error');
-const { mongoConnect } = require('./utils/databaseUtil');
-// require('./utils/databaseUtil');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
+app.use(express.urlencoded());
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-app.use(express.urlencoded());
 app.use(storeRouter);
 app.use("/host", hostRouter);
-
-app.use(express.static(path.join(rootDir, 'public')));
-
 app.use(ErrorPage)
 
+
 const PORT = 8000;
-mongoConnect(() => {
+const DB_PATH = "mongodb://sudhanshu1:coding12345@ac-ixqd2yz-shard-00-00.8bpirzw.mongodb.net:27017,ac-ixqd2yz-shard-00-01.8bpirzw.mongodb.net:27017,ac-ixqd2yz-shard-00-02.8bpirzw.mongodb.net:27017/airbnb?ssl=true&replicaSet=atlas-s0hxc6-shard-0&authSource=admin&appName=FirstCluster";
+
+mongoose.connect(DB_PATH).then(() => {
+    console.log("Connected to Mongo");
     app.listen(PORT, () => {
         console.log(`Server is running on: http://localhost:${PORT}`);
     })
+}).catch(err => {
+    console.log('Error while connected to mongo: ', err);
 })
