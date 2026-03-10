33f12913217b61b77f07c3c5ff491c3306004e56
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-10 13:23:31 +0530

    remove the commented code

--- database/02-introduction-to-mongodb/utils/databaseUtil.js
+++ database/02-introduction-to-mongodb/utils/databaseUtil.js
@@ -1,16 +1,3 @@
-// const mongoose = require("mongoose");
-
-// async function mongoConnect() {
-//   try {
-//     await mongoose.connect("mongodb://sudhanshu1:coding12345@ac-fhkeitf-shard-00-00.849qspp.mongodb.net:27017,ac-fhkeitf-shard-00-01.849qspp.mongodb.net:27017,ac-fhkeitf-shard-00-02.849qspp.mongodb.net:27017/?ssl=true&replicaSet=atlas-525nqb-shard-0&authSource=admin&appName=conding12345");
-//     console.log("MongoDB connected");
-//   } catch (err) {
-//     console.log("MongoDB connection error:", err);
-//   }
-// }
-// mongoConnect()
-
-
 const mongo = require('mongodb');
 
 const MangoClient = mongo.MongoClient;
