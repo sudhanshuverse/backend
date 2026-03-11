d699d524d8cc6ac4081d8423d6c466e282a7dc27
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-11 15:34:08 +0530

    Session error

--- database/04-cookies-and-sessions/controllers/error.js
+++ database/04-cookies-and-sessions/controllers/error.js
@@ -1,4 +1,8 @@
 exports.ErrorPage = (req, res, next) => {
     res.status(404);
-    res.render('404', {pageTitle: 'Page Not Found'})
+    res.render('404', {
+        pageTitle: 'Page Not Found',
+        currentPage: "404",
+        isLoggedIn: req.session.isLoggedIn
+    });
 }
