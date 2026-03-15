a83072f3299233b2cf98dc558fca62252e03505a
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-15 16:41:02 +0530

    Added todo backend folder

---
+++ database/07-rest-api-json-request/todo-backend/app.js
@@ -0,0 +1,27 @@
+// CORE MODULE
+const path = require('path');
+
+// EXTERNAL MODULE
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const DB_PATH = "mongodb://sudhanshu1:coding12345@ac-ixqd2yz-shard-00-00.8bpirzw.mongodb.net:27017,ac-ixqd2yz-shard-00-01.8bpirzw.mongodb.net:27017,ac-ixqd2yz-shard-00-02.8bpirzw.mongodb.net:27017/todo?ssl=true&replicaSet=atlas-s0hxc6-shard-0&authSource=admin&appName=FirstCluster";
+
+// LOCAL MODULE
+const errorsController = require('./controllers/errors');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
+app.use(errorsController.pageNotFound);
+
+const PORT = 8000;
+mongoose.connect(DB_PATH)
+  .then(() => {
+    console.log('Connected to Mongo');
+    app.listen(PORT, () => {
+      console.log(`Server is running on address http://localhost:${PORT}`);
+    });
+  }).catch(err => {
+    console.log('Error while connecting to Mongo', err);
+  })
